098c4ffc53c4f59411bf553f2e2cb48e7c2a7550
Sal Samani <sal@milysec.com>
2024-07-24 09:34:13 +1000

    redirect

--- src/app/burn/page.tsx
+++ src/app/burn/page.tsx
@@ -1,5 +1,17 @@
+'use client'
 import Image from 'next/image';
+import { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
+
 export default function Home() {
+    const url = "https://dial.to/?action=solana-action:https://run.fabs.fun/api/burn/";
+    const router = useRouter();
+
+    useEffect(() => {
+        // Redirect the user to the URL
+        router.push(url);
+    }, []);
+
     return (
         <div className="flex justify-center items-center h-screen">
             <Image src="/fabs-burn.png" alt="FABS.fun" width={256} height={256} />
