71fd5ca43b56cc8750d31ae6b5b70d2693343cb7
Sal Samani <sal@milysec.com>
2024-07-24 08:53:13 +1000

    mdx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,29 +1,19 @@
 export const metadata = {
   title: 'FABS.fun',
   description: 'Run, fun and FABS',
+  twitter: {
+    card: 'summary_large_image',
+    site: '@Sonarwatch',
+    title: 'FABS.fun',
+    description: 'Run, fun and FABS',
+    image: 'https://sonar.watch/banner.jpg',
+  },
 }
 
 export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+  children, params }: { children: React.ReactNode, params: any }) {
   return (
     <html lang="en">
-      <head>
-        <meta name="description"
-          content="SonarWatch is a multichain dashboard that supports you in monitoring your DeFi portfolio and staying updated on the web3 ecosystem in real time." />
-        <meta name="twitter:description"
-          content="SonarWatch is a multichain dashboard that supports you in monitoring your DeFi portfolio and staying updated on the web3 ecosystem in real time." />
-        <meta property="og:image" content="https://sonar.watch/banner.jpg" />
-        <meta name="twitter:image" content="https://sonar.watch/banner.jpg" />
-        <meta name="twitter:site" content="@Sonarwatch" />
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta property="og:type" content="website" />
-        <meta property="og:title" content="Sonar Watch, the multichain dashboard that empowers your DeFi journey" />
-        <meta property="og:site_name" content="Sonar Watch" />
-      </head>
-      <body>{children}</body>
     </html>
   )
 }
