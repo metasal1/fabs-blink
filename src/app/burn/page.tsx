6c21db29e51b9a2330f3e14e3815eb79195174af
Sal Samani <sal@milysec.com>
2024-07-24 08:40:08 +1000

    mdx

--- src/app/burn/page.tsx
+++
@@ -1,9 +0,0 @@
-import Image from "next/image";
-
-export default function Home() {
-    return (
-        <div>
-            <Image src="/fabs-burn.png" alt="FABS.fun" width={256} height={256} />
-        </div>
-    );
-}
