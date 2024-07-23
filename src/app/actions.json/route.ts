358af307fd6184fed09701dfac6e519850ff2b95
Sal Samani <sal@milysec.com>
2024-07-24 07:44:20 +1000

    new actions.json

--- src/app/actions.json/route.ts
+++ src/app/actions.json/route.ts
@@ -6,12 +6,12 @@ export const GET = async () => {
             // map all root level routes to an action
             {
                 pathPattern: "/*",
-                apiPath: "/api/**",
+                apiPath: "/burn/**",
             },
             // idempotent rule as the fallback
             {
-                pathPattern: "/api/**",
-                apiPath: "/api/**",
+                pathPattern: "/**",
+                apiPath: "/burn/**",
             },
         ],
     };
