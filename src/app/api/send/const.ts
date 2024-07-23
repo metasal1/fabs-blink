937957d3a0aa51f4d4eac6a47a3756f3ee387a15
Sal Samani <sal@milysec.com>
2024-07-23 17:54:51 +1000

    rpc

--- src/app/api/send/const.ts
+++ src/app/api/send/const.ts
@@ -1,7 +1,7 @@
 import { PublicKey } from "@solana/web3.js";
 
 export const DEFAULT_SOL_ADDRESS: PublicKey = new PublicKey(
-    "GaxVqiQyJKQDRu6H4pfy9V6Xq19pHGr6HQKDQDv911Y4", // devnet wallet
+    "GaxVqiQyJKQDRu6H4pfy9V6Xq19pHGr6HQKDQDv911Y4",
 );
 
 export const DEFAULT_SPL_AMOUNT: number = 1.0;
